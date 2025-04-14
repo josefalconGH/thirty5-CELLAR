b2facb920cf88963b48ccef4a35fa0549f549561
Jose Falcon <jfalconpriv@gmail.com>
2025-04-14 16:30:11 -0500

    adds vite config, es lint config

---
+++ client/vite.config.js
@@ -0,0 +1,22 @@
+// Purpose: Vite configuration
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    proxy: {
+      "/graphql": {
+        target: "http://localhost:3001",
+        changeOrigin: true,
+        secure: false,
+      },
+      "/api": {
+        target: "http://localhost:3001",
+        changeOrigin: true,
+        secure: false,
+      },
+    },
+  },
+});

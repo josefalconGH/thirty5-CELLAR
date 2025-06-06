46b2b589a9b1770cf0ab5b0b54d4a6882124fae8
Jose Falcon <jfalconpriv@gmail.com>
2025-06-06 16:34:34 -0500

    adds src common assets ts file

---
+++ client/src/common/assets.ts
@@ -0,0 +1,14 @@
+export const ASSET_PACK_KEYS = {
+    MAIN: 'MAIN',
+  } as const;
+  
+  export const ASSET_KEYS = {
+    FONT_PRESS_START_2P: 'FONT_PRESS_START_2P',
+  } as const;
+  
+  export const PLAYER_ANIMATION_KEYS = {
+  } as const;
+  
+  export const CHARACTER_ANIMATIONS = {
+  } as const;
+  

5373c946b55f209fbdfe6a3a3fd844fbb64bbda3
Jose Falcon <jfalconpriv@gmail.com>
2025-06-06 16:07:44 -0500

    updates vite confige to include entry file names for assets

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,7 +4,15 @@ import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "./",
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      ouput: {
+        entryFileNames: 'assets/js/[name]-[hash].js',
+      },
+    },
+  },
   server: {
     proxy: {
       "/graphql": {

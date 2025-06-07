372be0e8be6c4003f806485df590342ce849689b
Jose Falcon <jfalconpriv@gmail.com>
2025-06-06 19:16:50 -0500

    responsive game-scene text

--- client/src/scenes/game-scene.ts
+++ client/src/scenes/game-scene.ts
@@ -4,6 +4,8 @@ import { SCENE_KEYS } from './scene-keys';
 import { ASSET_KEYS } from '../common/assets';
 
 export class GameScene extends Phaser.Scene {
+  private titleText!: Phaser.GameObjects.Text;
+
   constructor() {
     super({
       key: SCENE_KEYS.GAME_SCENE,
@@ -11,10 +13,20 @@ export class GameScene extends Phaser.Scene {
   }
 
   public create(): void {
-    this.add
+    this.titleText = this.add
       .text(this.scale.width / 2, this.scale.height / 2, 'Game Scene', {
         fontFamily: ASSET_KEYS.FONT_PRESS_START_2P,
       })
       .setOrigin(0.5);
+
+    this.scale.on('resize', this.resize, this);
+  }
+
+  private resize(gameSize: Phaser.Structs.Size): void {
+    const { width, height } = gameSize;
+
+    if (this.titleText) {
+      this.titleText.setPosition(width / 2, height / 2);
+    }
   }
 }
